a1880719b0e6035d9267e6f24e5bdc2ba3457936
Gerardo Merida <cgemerida@gmail.com>
2023-05-09 19:57:22 -0600

    add custom actions

---
+++ .github/actions/js-test/main.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+const run = () => {
+  // Get values
+  const path = core.getInput('path', {
+    required: true,
+    trimWhitespace: true,
+  });
+
+  exec(`echo "Testing js actions where path is ${path}"`)
+};
+
+run();
